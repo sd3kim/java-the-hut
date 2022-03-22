90c4bb15d1d9862a447ae05ad613255a94ff55ec
zara sultan <zarasultan@zaras-MacBook-Pro.local>
2022-03-22 11:57:43 -0400

    adding the javascript to buttons

--- public/javascripts/drink.js
+++ public/javascripts/drink.js
@@ -2,13 +2,6 @@ const add = document.getElementById("ad");
 const sub = document.querySelector(".sub");
 const input = document.querySelector(".input");
 let total, inputVal;
-
-// function adding() {
-//   inputVal = parseInt(input.value);
-//   total = inputVal + 1;
-//   input.textContent = total;
-//   console.log("total", total);
-// }
 function adding() {
   inputVal = input.valueAsNumber++;
   input.textContent = inputVal;
