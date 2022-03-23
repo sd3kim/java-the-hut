ff6f9b6382c1a37f613ca2c17f8880353ad122c6
sd3kim <sd3kim@gmail.com>
2022-03-22 20:18:58 -0400

    Add functionality to customization options

--- public/javascripts/customization-button.js
+++ public/javascripts/customization-button.js
@@ -1,19 +1,81 @@
-const add = document.querySelectorAll(".add");
-const sub = document.querySelectorAll(".sub");
-const input = document.querySelectorAll(".input");
+// const add = document.querySelectorAll(".add");
+// const sub = document.querySelectorAll(".sub");
+// const input = document.querySelectorAll(".input");
 
-let total, inputVal;
-function adding() {
-  input.forEach(function (evt) {
-    evt.textContent = evt.valueAsNumber++;
-  });
-}
-function substract() {
-  inputVal = input.valueAsNumber--;
-  input.textContent = inputVal;
-}
-add.forEach(function (evt) {
-  evt.addEventListener("click", adding);
-});
+// let total, inputVal;
+// function adding() {
+//   input.forEach(function (evt) {
+//     evt.textContent = evt.valueAsNumber++;
+//   });
+// }
+// function substract() {
+//   inputVal = input.valueAsNumber--;
+//   input.textContent = inputVal;
+// }
+// add.forEach(function (evt) {
+//   evt.addEventListener("click", adding);
+// });
 
 // sub.addEventListener("click", substract);
+
+const subButtonCreamEl = document.getElementById("sub-cream");
+const addButtonCreamEl = document.getElementById("add-cream");
+const subButtonMilkEl = document.getElementById("sub-milk");
+const addButtonMilkEl = document.getElementById("add-milk");
+const subButtonSugarEl = document.getElementById("sub-sugar");
+const addButtonSugarEl = document.getElementById("add-sugar");
+
+let totalCream = 0;
+let totalMilk = 0;
+let totalSugar = 0;
+
+// add/sub cream
+function subCreamTotal() {
+  if (totalCream <= 0) {
+    return;
+  } else {
+    totalCream--;
+  }
+  updateCountDisplay();
+}
+
+function addCreamTotal() {
+  totalCream++;
+  updateCountDisplay();
+}
+
+// add/sub milk
+function subMilkTotal() {
+  if (totalMilk <= 0) {
+    return;
+  } else {
+    totalMilk--;
+  }
+  updateCountDisplay();
+}
+
+function addMilkTotal() {
+  totalMilk++;
+  updateCountDisplay();
+}
+
+// add/sub sugar
+function subSugarTotal() {
+  if (totalSugar <= 0) {
+    return;
+  } else {
+    totalSugar--;
+  }
+  updateCountDisplay();
+}
+
+function addSugarTotal() {
+  totalSugar++;
+  updateCountDisplay();
+}
+
+function updateCountDisplay() {
+  document.getElementById("input-cream").value = totalCream;
+  document.getElementById("input-milk").value = totalMilk;
+  document.getElementById("input-sugar").value = totalSugar;
+}
