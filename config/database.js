f4aa43f4ed556c278002806b92c85c3eaaf0b495
sd3kim <sd3kim@gmail.com>
2022-03-24 17:38:16 -0400

    Update connection string to pool

--- config/database.js
+++ config/database.js
@@ -1,14 +1,12 @@
-// npm i --save pg
-//destructure Pool from pg
 const { Pool } = require("pg");
 //create a new Pool connection with our credentials
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABSE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
