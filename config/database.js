39c720f59a754c765c4025fb43c6569d3dc8536c
sd3kim <sd3kim@gmail.com>
2022-03-24 20:29:40 -0400

    Update README and config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
+// npm i --save pg
+//destructure Pool from pg
 const { Pool } = require("pg");
-const connectionString =
-  "postgresql://dbuser:secretpassword@database.server.com:3211/mydb";
-
+//create a new Pool connection with our credentials
 const pool = new Pool({
-  connectionString: process.env.DATABSE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
-
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
